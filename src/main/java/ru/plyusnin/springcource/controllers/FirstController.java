5c8063c3f066348291c57d0d5a9c1b1b8b54a083
ne91en <141094530+ne91en@users.noreply.github.com>
2023-08-06 16:38:34 +0800

    initial commit

--- src/main/java/ru/plyusnin/springcource/controllers/FirstController.java
+++ src/main/java/ru/plyusnin/springcource/controllers/FirstController.java
@@ -10,9 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest httpServletRequest) {
-        String name = httpServletRequest.getParameter("name");
-        String surname = httpServletRequest.getParameter("surname");
+    public String helloPage(@RequestParam(value = "name", required = false) String name,@RequestParam(value = "surname", required = false) String surname ) {
+
         System.out.println("hello " + name + " " + surname);
         return "first/hello";
     }
