f5b03312aa56445b1ad5dd5edd7f3480619bac25
ne91en <141094530+ne91en@users.noreply.github.com>
2023-08-06 17:23:57 +0800

    initial commit

--- src/main/java/ru/plyusnin/springcource/controllers/FirstController.java
+++ src/main/java/ru/plyusnin/springcource/controllers/FirstController.java
@@ -2,6 +2,7 @@ package ru.plyusnin.springcource.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,13 +11,35 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name", required = false) String name,@RequestParam(value = "surname", required = false) String surname ) {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        System.out.println("hello " + name + " " + surname);
+//        System.out.println("hello " + name + " " + surname);
+        model.addAttribute("message", "hello " + name + " " + surname);
         return "first/hello";
     }
     @GetMapping("/goodbye")
     public String goodByePage(){
         return "first/goodbye";
+    }
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a,
+                             @RequestParam("b") int b,
+                             @RequestParam("action") String action,
+                             Model model){
+        int sum = 0;
+        switch (action){
+            case "additional": sum=a+b;
+            break;
+            case "subtraction": sum=a-b;
+            break;
+            case "multiplication" : sum=a*b;
+            break;
+            case "division" : sum=a/b;
+            break;
+        }
+        model.addAttribute("sum", sum);
+        return "first/calculator";
     }
 }
