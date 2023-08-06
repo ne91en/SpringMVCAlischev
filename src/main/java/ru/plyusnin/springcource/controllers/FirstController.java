01d56b4704bf15d6bbe995d935ab9a4c86263d37
ne91en <141094530+ne91en@users.noreply.github.com>
2023-08-06 16:24:05 +0800

    initial commit

--- src/main/java/ru/plyusnin/springcource/controllers/FirstController.java
+++ src/main/java/ru/plyusnin/springcource/controllers/FirstController.java
@@ -2,11 +2,15 @@ package ru.plyusnin.springcource.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@RequestMapping("/first")
 @Controller
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam("name") String name) {
+        System.out.println(name);
         return "first/hello";
     }
     @GetMapping("/goodbye")
