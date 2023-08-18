6555e3be9459a27f005e35f142112cf39b4a9f65
ne91en <141094530+ne91en@users.noreply.github.com>
2023-08-18 13:58:18 +0800

    VALID

--- src/main/java/ru/plyusnin/springcource/dao/PersonDAO.java
+++ src/main/java/ru/plyusnin/springcource/dao/PersonDAO.java
@@ -1,6 +1,7 @@
 package ru.plyusnin.springcource.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.plyusnin.springcource.models.Person;
@@ -20,12 +21,12 @@ public class PersonDAO {
 
     /////////////////////////////////////////////
     public List<Person> index(){
-        return jdbcTemplate.query("SELECT * FROM Person", new PersonMapper() );
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class) );
     }
     /////////////////////////////////////////////
     public Person show(int id){
 
-        return jdbcTemplate.query("SELECT * FROM Person WHERE id = ?", new Object[]{id},new PersonMapper())
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id = ?", new Object[]{id},new BeanPropertyRowMapper<>(Person.class))
                 .stream().findAny().orElse(null);
     }
     /////////////////////////////////////////////
